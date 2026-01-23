735cd1f6913cb0262fbee39684043d4c741339ab
Tran Ngoc Tan Dat <21130314@st.hcmuaf.edu.vn>
2026-01-23 09:43:13 +0700

    changed footer

--- src/layout/MasterLayout/MasterLayout.tsx
+++ src/layout/MasterLayout/MasterLayout.tsx
@@ -1,13 +1,12 @@
 import Header from "../Header/Header";
 import { Outlet } from "react-router-dom";
-import Footer from "../Footer/Footer";
 
 const MasterLayout = () => {
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
       <Outlet />
-      <Footer />
+  
     </div>
   );
 };
